c765ae8e916a5579fb8d5c3d833be5a2b62d2809
rbbit25 <rburakboz@gmail.com>
2020-07-12 01:54:57 +0000

    dockerbuild

---
+++ example.groovy
@@ -0,0 +1,17 @@
+def name= "Rbb" // String Type
+
+def names = ["Anna", "RBB", "Vova"]   // List of Strings
+
+def myinfo = [
+    "name" : "Rb", 
+    "last_name" : "B"
+    ]    // Map in groovy
+
+def debugMode = false // Bool value in groovy
+
+def sayHello() {   // Functions in groovy
+    println('Hello World')
+}
+
+sayHello()   // call the function in groovy
+println(myinfo) 

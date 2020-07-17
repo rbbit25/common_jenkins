b0a5702086b4145265ff15fade21154a4215ea2f
rbbit25 <rburakboz@gmail.com>
2020-07-17 18:31:54 +0000

    github-task update

--- github_task.groovy
+++ github_task.groovy
@@ -17,17 +17,8 @@ def slavePodTemplate = """
                   - jenkins-jenkins-master
               topologyKey: "kubernetes.io/hostname"
         containers:
-        - name: fuchicorp
+        - name: fuchicorptools
           image: fuchicorp/buildtools:latest
-          imagePullPolicy: IfNotPresent
-          command:
-          - cat
-          tty: true
-          volumeMounts:
-            - mountPath: /var/run/docker.sock
-              name: docker-sock
-        - name: docker
-          image: docker:latest
           imagePullPolicy: IfNotPresent
           command:
           - cat
@@ -45,30 +36,26 @@ def slavePodTemplate = """
               path: /var/run/docker.sock
     """
     properties([
-        parameters([choice(choices: ['fsadykov', 'antonbabenko', 'mojombo', 'defunkt'], 
-        description: 'Select specific user to run the Script2', name: 'github-user')])
-    ])
-     podTemplate(name: k8slabel, label: k8slabel, yaml: slavePodTemplate, showRawYaml: false) {
+        parameters([
+            choice(choices: ['fsadykov', 'antonbabenko', 'mojombo', 'defunkt'], 
+            description: 'please select github username', name: 'Github_users')
+        ])
+        ])
+    podTemplate(name: k8slabel, label: k8slabel, yaml: slavePodTemplate, showRawYaml: false) {
       node(k8slabel) {
-
-    container('fuchicorp'){
-       withCredentials([string(credentialsId: 'github-token', variable: 'GIT_TOKEN')]) {
-            stage("Check Creds"){
-                sh """
-                    #!/bin/bash
-                curl -H "Authorization: token $GIT_TOKEN" -X GET 'https://api.github.com/users' -I |grep 'HTTP/1.1 200 OK'
-                    """ 
-
-                }
-            stage("Get User"){
-            println("Specific user is: ${params.github-user}")
-            sh  """
-                    #!/bin/bash
-                curl -H "Authorization: token $GIT_TOKEN" -X GET 'https://api.github.com/users/${params.github-user}'
-                """ 
-                }
-           }
-      
+        withCredentials([string(credentialsId: 'github_token', variable: 'GIT_TOKEN')]) {
+      stage("Get User") {
+            println("Selected Github user is: ${Github_users}")
+            println('Building the github-task')  
+            container("fuchicorptools") {
+                sh "curl -H  \"Authorization: token $GIT_TOKEN \" -X GET 'https://api.github.com/users/${Github_users}'"
+            }
         }
-      }
+      stage("Check Creds") {
+           container("fuchicorptools") {
+                sh 'curl -H "Authorization: token $GIT_TOKEN " -X GET "https://api.github.com/users" -I | grep "HTTP/1.1 200 OK" '
+                }
+            }
+      }                         
+     }
     }
